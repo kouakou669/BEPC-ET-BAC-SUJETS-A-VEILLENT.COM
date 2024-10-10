e1d9cad96f7191e468cbcef5519b1b5b42e4824a
kouakou669 <kouamedjakiss766@gmail.com>
2024-10-10 23:01:48 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function showNotification() {
     const snackbar = document.getElementById("snackbar");
     snackbar.className = "show"; // Ajoute la classe pour afficher la notification
-    setTimeout(() => { snackbar.className = snackbar.className.replace("show", ""); }, 3000); // Cache après 3 secondes
+    setTimeout(() => { snackbar.className = snackbar.className.replace("show", ""); }, 6000); // Cache après 3 secondes
 }
